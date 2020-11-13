8d3ef23d8a49c42134393a565ee2419c46674287
Mauricio Robayo <rfmajo@gmail.com>
2020-11-13 06:39:19 -0500

    Cascading middleware

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const Koa = require("koa")
 
 const app = new Koa();
 
-app.context.date = new Date();
+app.context.userData = {
+  first: 'Mauricio',
+  last: 'Robayo',
+}
 
-app.use(ctx => {
-  ctx.state.user = 'Mauricio'
-  ctx.state.date = new Date();
-  ctx.body = `Hello ${ctx.state.user}, ${ctx.date}, ${ctx.state.date}`
+// log
+app.use(async (ctx, next) => {
+  await next();
+  const responseTime = ctx.response.get('X-Response-Time')
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${responseTime}`)
+})
+
+app.use(async (ctx, next)=> {
+  const start = Date.now()
+  await next()
+  const responseTime = Date.now() - start
+  ctx.set(`X-Response-Time`, `${responseTime}ms`)
+})
+
+app.use(async (ctx) => {
+  ctx.response.body = await ctx.userData;
 })
 
 app.listen(3000);
